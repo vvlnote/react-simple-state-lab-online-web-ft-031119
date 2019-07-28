6504ed0f362ef95a29afcb2c01853e0a06ad01ef
Joy Lin <vvlnote@hotmail.com>
2019-07-28 22:57:00 +0000

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Matrix extends Component {
-  
+
   genRow = (vals) => (
     vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
   )
-  
+
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
-  
+
   render() {
     return (
       <div id="matrix">
@@ -17,5 +17,39 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+
 }
+
+Matrix.defaultProps ={
+  values: (() => {
+    let arr = [][];
+    for (i = 0; i < 10; i++) {
+      for (j=0; j < 10; j++){
+        arr[i][j] = '#F00';
+      }
+    }
+    return arr;
+  })();
+//   for (i = 0; i < 10; i++) {
+//     for (j = 0; j < 10; j++){
+//       values[i][j] = '#F00';
+//     }
+//   }
+//
+//   values: (() => {
+//   const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   return (new Array(10).fill(defRow))
+// })()
+  // values = [
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F'],
+  //   ['#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F', '#F00', '#00F']
+  // ]
+};
